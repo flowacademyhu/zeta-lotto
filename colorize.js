89e61998e5cd3a75cbe5beb010363236e2295ee1
Kinga <hkingaa@gmail.com>
2019-06-19 15:06:29 +0200

    colorize numbers

---
+++ colorize.js
@@ -0,0 +1,10 @@
+const colorize = (num) => {
+  if (num % 2 === 0) {
+    return ('\x1b[32m' + num + '\x1b[0m');
+  } else {
+    return ('\x1b[31m' + num + '\x1b[0m');
+  }
+};
+
+console.log(colorize(6));
+console.log(colorize(5));
